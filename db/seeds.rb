504e3354255eae8091388ce636c55488aba45993
Kevin Kotcherga <kevin.kotcherga@gmail.com>
2022-02-03 11:27:30 +0100

    ✨feat(react): nouvelle adresse pour récuperer l'API rails

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Book.create(title: "Ruby is amazing", body: "I learned how to build an ORM and use a database with a mix of SQL and Ruby")
+Book.create(title: "React is a great and dynamic front end technology", body: "Now I know how to build a full stack application using Rails for backend and React for the frontend")
+Book.create(title: "Deploying today", body: "Today I am learning how to deploy an application to the web")
